54182ad5d04e630ae08b4be1f04b3b1e2f3eb0c1
bjm00 <baijiaming_1998@163.com>
2019-07-29 19:38:43 +0800

    Initial commit

---
+++ src/com/pojo/subject1/Soulution.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: Soulution
+ * Author:   think
+ * Date:     2019/7/29 17:01
+ * Description: 剑指offer第一题
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.pojo.subject1;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈剑指offer第一题〉
+ *
+ * @author think
+ * @create 2019/7/29
+ * @since 1.0.0
+ */
+public class Soulution {
+
+}
