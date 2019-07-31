aae545a601d3a851250f2867f5ad9d6b1d362bf6
bjm00 <baijiaming_1998@163.com>
2019-07-31 19:41:14 +0800

    剑指offer第一题第二题解法

--- src/com/pojo/subject1/Soulution.java
+++ src/com/pojo/subject1/Soulution.java
@@ -10,14 +10,32 @@
  */
 package com.pojo.subject1;
 
+
 /**
- * 〈一句话功能简述〉<br> 
- * 〈剑指offer第一题〉
+ * 〈暴力遍历寻找 时间复杂度n^*2〉<br>
+ * 〈剑指offer第一题
+ * 在一个二维数组中（每个一维数组的长度相同），
+ * 每一行都按照从左到右递增的顺序排序，每一列都按照
+ * 从上到下递增的顺序排序。请完成一个函数，输入这样的
+ * 一个二维数组和一个整数，判断数组中是否含有该整数。〉
  *
  * @author think
  * @create 2019/7/29
  * @since 1.0.0
  */
 public class Soulution {
-
+        public boolean find(int target,int [][] array){
+            //Scanner scanner = new Scanner(System.in);
+            if (array == null) {
+                return false;
+            }else{
+                for (int i = 0; i < array.length; i++) {
+                    for (int j = 0; j < array[i].length; j++) {
+                        if (array[i][j] == target)
+                            return true;
+                    }
+                }
+            }
+            return false;
+        }
 }
